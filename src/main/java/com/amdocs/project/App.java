06e8ee04bbcbb8a249caf6d346a2c331fc60a5eb
wasih707 <abdulwasihbekal@gmail.com>
2023-08-17 15:53:28 +0530

    First Commit

---
+++ src/main/java/com/amdocs/project/App.java
@@ -0,0 +1,16 @@
+package com.amdocs.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    } 
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
